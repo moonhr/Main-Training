e32f72866d45f72c2acc35323f3f8b295edbf51c
moonhr <moonhr153@gmail.com>
2024-11-13 11:02:09 +0900

    test 문법 연습

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -8,3 +8,11 @@ test("1+2 = 3", () => {
 test("5+7=12", () => {
   expect(sum(5, 7)).toBe(12);
 });
+
+test("number comparisons", () => {
+  expect(5 + 5).toBe(10); // ===로 비교
+  expect({ a: 1 }).toEqual({ a: 1 }); //객체 구조가 같은지 비교
+  expect(null).toBeNull(); // 값이 null인지 비교
+  expect([1, 2, 3]).toContain(2); // 배열에 특정 값이 포함되었는지 비교
+  expect("Hello").toMatch(/Hello/); // 정규식 일지 확인
+});
